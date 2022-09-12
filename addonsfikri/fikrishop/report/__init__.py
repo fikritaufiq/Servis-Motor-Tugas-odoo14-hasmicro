27ad6c1d7f6590c41eb417aa06326a7a17f21fc0
Fikri <fikri@gmail.com>
2022-09-12 23:30:34 +0700

    first commit

---
+++ addonsfikri/fikrishop/report/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import grup
+from . import manusia
+from . import pelanggan
+from . import produk
+from . import pemasok
+from . import penjualan
+from . import barang
+from . import pembelian
+from . import pengguna
+from . import kasir
+from . import manager
+#from . import pembeliandetail
